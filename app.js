3c01f90e61a84df1aa262973bccdd13a5e814f16
Karan Verma <Karan.Verma@pb.com>
2016-12-21 17:40:49 +0530

    MyProfile commit.

---
+++ app.js
@@ -0,0 +1,40 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  ,favicon = require('serve-favicon')
+  ,con = require('consolidate');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 4000);
+app.engine('html', con.swig);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.use(favicon(__dirname+'/public/img/fevicon.ico'));
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+//app.get('/dwnldRsm', '');
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+
+});
---
+++ public/js/custom.js
@@ -0,0 +1,8 @@
+/**
+ * http://usejsdoc.org/
+ */
+$('navbar-main-collapse').on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
